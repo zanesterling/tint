ed223ecba3948b60bd966109cdc902e336184b1c
Zane Sterling <sterling.zane@gmail.com>
2014-09-20 11:26:59 -0400

    Set up simple homepage

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import secrets
 
 app = Flask(__name__)
 app.secret_key = "blerp derp"
 
 @app.route('/')
 def home():
-	return "Eyyyy Tint"
+	d = {'client-id': secrets.client_id}
+	return render_template("home.html", d=d)
 
 if __name__ == "__main__":
 	app.run("0.0.0.0", debug=True)
