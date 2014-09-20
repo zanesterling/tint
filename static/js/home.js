aa2a1f1e925bed680427c51e5595a98e056e7fce
Zane Sterling <sterling.zane@gmail.com>
2014-09-20 17:49:07 -0400

    Solidify classes for repos

--- static/js/home.js
+++ static/js/home.js
@@ -5,16 +5,23 @@ $(function() {
 	for (var i = 0; i < tintRepos.length; i++) {
 		for (var j = 0; j < shownRepos.length; j++) {
 			if (tintRepos[i]['name'] == shownRepos[j].textContent()) {
-				$(shownRepos[j]).addClass('tintified');
+				$(shownRepos[j]).addClass('tinted');
 			}
 		}
 	}
 
+	for (var i = 0; i < shownRepos.length; i++) {
+		if (!$(shownRepos[j]).hasClass('tintified')) {
+			$(shownRepos[j]).addClass('untinted');
+		}
+	}
+
 	$('.repo').click(function(event) {
-		if ($(event.target).hasClass('tintified'))
+		if ($(event.target).hasClass('tinted'))
 			return;
 
 		a = event.target;
+		$(event.target).removeClass('untinted');
 		$(event.target).addClass('tinting');
 		$.get(window.location.origin + '/client-callback',
 			{
@@ -23,8 +30,8 @@ $(function() {
 				'repo': $(event.target).text()
 			},
 			function() {
-				$(event.target).addClass('tinted');
 				$(event.target).removeClass('tinting');
+				$(event.target).addClass('tinted');
 			}
 		);
 	});
