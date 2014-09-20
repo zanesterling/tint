46d89a3f4691d91c2ad695ddd4ba260a75b84d56
Zane Sterling <sterling.zane@gmail.com>
2014-09-20 12:00:21 -0400

    Add sign in with GitHub

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
+import requests as pyrequests
 import secrets
 
 app = Flask(__name__)
@@ -8,6 +9,17 @@ app.secret_key = "blerp derp"
 def home():
 	d = {'client-id': secrets.client_id}
 	return render_template("home.html", d=d)
+
+@app.route('/oauth-callback')
+def oauth_callback():
+	data = {}
+	data["client_id"] = secrets.client_id
+	data["client_secret"] = secrets.client_secret
+	data["code"] = request.args.get("code")
+	r = pyrequests.post('https://github.com/login/oauth/access_token', data)
+	session['github_token'] = r.text
+
+	return redirect(url_for('home'))
 
 if __name__ == "__main__":
 	app.run("0.0.0.0", debug=True)
