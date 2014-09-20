5e9755398a5f7acd176d8fca6e0570209224e9ef
Josh Hofing <joshhofing@gmail.com>
2014-09-20 12:09:39 -0400

    Switch to tabs

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app.secret_key = "blerp derp"
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    print json.loads(request.data)
-    return "AY"
+	print json.loads(request.data)
+	return "AY"
 
 if __name__ == "__main__":
 	app.run("0.0.0.0", debug=True)
