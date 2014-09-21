38b2ffc6b067cde1c8de866d04632d9fc1cf9061
Zane Sterling <sterling.zane@gmail.com>
2014-09-20 22:32:33 -0400

    Add untinting

--- app_extension.py
+++ app_extension.py
@@ -29,5 +29,23 @@ def tintRepo(username, oauth_token, reponame):
 
 	# add tintapplication as collaborator
 	headers = {'Content-Length': 0}
-	r = requests.put('https://api.github.com/repos/%s/%s/collaborators/tintapplication?%s' %
+	requests.put('https://api.github.com/repos/%s/%s/collaborators/tintapplication?%s' %
 		         (username, reponame, oauth_token), headers=headers)
+
+def untintRepo(username, oauth_token, reponame):
+	# get id of tint webhook
+	r = requests.get('https://api.github.com/repos/%s/%s/hooks?%s' % (username, reponame, oauth_token))
+	hooks = [hook['id'] for hook in json.loads(r.text)
+		     if hook['config']['url'] == 'http://localhost:5000/webhook']
+	if len(hooks):
+		hookid = hooks[0]
+
+		# remove tint webhook
+		headers = {'Content-Length': 0}
+		requests.delete('https://api.github.com/repos/%s/%s/hooks/%i?%s' %
+			            (username, reponame, hookid, oauth_token), headers=headers)
+
+	# add tintapplication as collaborator
+	headers = {'Content-Length': 0}
+	requests.delete('https://api.github.com/repos/%s/%s/collaborators/tintapplication?%s' %
+		            (username, reponame, oauth_token), headers=headers)
